6b6b8a7642703ee7795745cc20f4bfefc106f6b0
Glenn Tweedie <glenn.tweedie@digitaldialogue.com.au>
2014-11-20 13:30:28 +0800

    tidy up seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,15 @@
+
+Rover.delete_all if Rails.env.test?
+
 # there should be six rovers, the vehicle_id is defined by the
 # external locator system, so can't be the primary key
-Rover.delete_all if Rails.env.test?
 (1..6).each do |vid|
   Rover.find_or_create_by(vehicle_id:vid)
 end
 
+
 apollo11site = LunarLocation.find_or_create_by(lat:0.681400, long: 23.460550)
+
 ['CND Command Centre','Apollo 11 Landing Site'].each do |name|
   PointOfInterest.find_or_create_by(name:name) do |poi|
     poi.location = apollo11site
