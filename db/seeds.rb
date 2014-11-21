e8d35e8db33f01b3ab5606dbcb4b8eff59aa0275
Glenn Tweedie <glenn.tweedie@digitaldialogue.com.au>
2014-11-21 16:16:52 +0800

    rover vehicle IDs are 0..5 not 1..6

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ Rover.delete_all if Rails.env.test?
 
 # there should be six rovers, the vehicle_id is defined by the
 # external locator system, so can't be the primary key
-(1..6).each do |vid|
+(0..5).each do |vid|
   Rover.find_or_create_by(vehicle_id:vid)
 end
 
