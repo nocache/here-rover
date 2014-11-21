417ca1866257ca704f30d1893b77108cd63fadb4
Glenn Tweedie <glenn.tweedie@digitaldialogue.com.au>
2014-11-21 16:14:53 +0800

    add Placemark model
    kind of a view model for rover instances

---
+++ app/models/placemark.rb
@@ -0,0 +1,31 @@
+require 'forwardable'
+
+class Placemark
+  extend Forwardable
+  attr_accessor :place
+
+  def initialize(place=nil)
+    @place = place
+  end
+
+  def self.build_list(places=[])
+    Array(places).collect do |place|
+      new(place)
+    end
+  end
+
+  def coords
+    lat, long = 0.0, 0.0
+    if @place.respond_to? :latest_location
+      lat = @place.latest_location.try :lat
+      long = @place.latest_location.try :long
+    end
+    "#{lat},#{long}"
+  end
+
+  def_delegator :@place, :vehicle_id, :id
+
+  def name
+   "#{@place.class.name} #{self.id}"
+  end
+end
