05a00ea60bafc63de608598c2c8552e5efb4de2d
Glenn Tweedie <glenn.tweedie@digitaldialogue.com.au>
2014-11-22 09:26:45 +0800

    redirect from root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'locations#index'
+  root :to => redirect('locations#index')
   get 'locations' => 'locations#index'
   get 'locations/rover/:vehicle_id' => 'locations#show', :as => :rover
 
