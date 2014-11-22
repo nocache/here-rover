614b5f4d56ad5c6ecbc05b1ba283e492e310b6bc
Glenn Tweedie <glenn.tweedie@digitaldialogue.com.au>
2014-11-22 09:20:13 +0800

    name the route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'locations#index'
   get 'locations' => 'locations#index'
-  get 'locations/rover/:vehicle_id' => 'locations#show'
+  get 'locations/rover/:vehicle_id' => 'locations#show', :as => :rover
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
