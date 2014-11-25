0f879307c107753c2bcb6a3ce0c74f7044f44b04
Glenn Tweedie <glenn.tweedie@digitaldialogue.com.au>
2014-11-25 09:56:40 +0800

    fix bug in distance calculator

--- app/models/lunar_location.rb
+++ app/models/lunar_location.rb
@@ -8,13 +8,14 @@ class LunarLocation < Location
     # result in is metres
     lat1_r = to_rad(self.lat)
     lat2_r = to_rad(other.lat)
-    delta = to_rad(other.lon - self.lon)
+    delta = to_rad(other.long - self.long)
     Math.acos(
       Math.sin(lat1_r) * Math.sin(lat2_r) +
       Math.cos(lat1_r) * Math.cos(lat2_r) * Math.cos(delta)
     ) * RADIUS
   end
 
+  private
   def to_rad(f)
     f / 180 * Math::PI
   end
