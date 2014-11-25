c0ddeb20f8469e99134c07dd4d53c5eaf12658a2
Glenn Tweedie <glenn.tweedie@digitaldialogue.com.au>
2014-11-25 09:57:04 +0800

    add distance to CND cmd centre to description in kml:

--- app/models/placemark.rb
+++ app/models/placemark.rb
@@ -26,6 +26,16 @@ class Placemark
     "#{long},#{lat}"
   end
 
+  def description
+    if @place && @place.respond_to?(:latest_location)
+      if @place.latest_location.respond_to?(:distance_to)
+        if ( dist = @place.latest_location.distance_to(@command_centre_poi.try(:location)) )
+          "Distance from CND Command Centre: #{ dist.round(1) } km"
+        end
+      end
+    end
+  end
+
   def name
    "#{@place.class.name} #{self.id}"
   end
